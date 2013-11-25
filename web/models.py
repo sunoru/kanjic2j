79eb9c2ff71ea144739994a46847a298a67cb2d0
sunoru <s@sunoru.com>
2013-11-25 19:01:45 +0800

    public-6

--- web/models.py
+++ web/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Log(models.Model):
 	filename = models.CharField(max_length=30)
-	use_time = models.TimeField()
+	use_time = models.DateTimeField()
 	address = models.IPAddressField()
 	def __unicode__(self):
 		return self.filename.replace('\n','')+'\t'+self.address
