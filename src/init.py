d7edb01900a7414fd5603d96bd3ca03bdf3e9993
sunoru <s@sunoru.com>
2013-12-01 11:12:41 +0800

    fixed some bugs, publish-6

--- src/init.py
+++ src/init.py
@@ -20,6 +20,21 @@ def init():
 	fout.close()
 	print 'Done.'
 
+def edit():
+	import cPickle as cp
+	fin=file('kanjic2j/kanjic2j_xcj.dat')
+	xcj=cp.load(fin)
+	fin.close()
+	for e1,e2 in xcj.items():
+		if len(e2)>1 and e2[0]==e1:
+			tt=e2[1]
+			e2[1]=e2[0]
+			e2[0]=tt
+	fout=file('kanjic2j/kanjic2j_xcj.dat','wb')
+	cp.dump(xcj,fout)
+	fout.close()
+	print 'Done!'
 if __name__=='__main__':
-	init()
+	#init()
+	edit()
 
